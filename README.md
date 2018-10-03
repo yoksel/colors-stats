8ae9656aec39e30d6b5c84c4ca074adf9b1f6aca
yokselzok <yokselzok@gmail.com>
2018-10-03 20:22:45 +0300

    Update readme & config example

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Colors stats
 
-Tool finds all colors in CSS and shows it on one page with usage stats & files urls by hover
+Tool finds all colors in CSS/JS and shows it on one page with usage stats & files urls by hover
 
 1. Place it to your project
 2. Rename `config-example` to `config` and fill it with the actual data
 3. Run `node YOUR_PATH/colorsStats/index.js`
 4. Open in browser generated `index.html` (you will find it in `colorsStats` folder)
+
+Tool can optionally:
+
+* ignore files and directories
+* filter files by date
+* parse only given urls
+
+See `config-example.json` for more information
--- config-example.js
+++ config-example.js
@@ -1,11 +1,28 @@
 module.exports = {
+  // Initial path to start parsing files and directories
   initialPath: 'styles',
 
-  ignoreDirs: [
-    '/old-version'
-
+  // if exist, initialPath will be ignored,
+  // search will be started from last of items
+  setDirsToParse: [
+    'my-fancy-component/styles',
+    'another-fancy-component',
   ],
 
+  // If exist, files older than this date
+  // will not be processed
+  notOlderThan: {
+    year: '2017',
+    month: '1', //1-12,
+    day: '1' //1-32
+  },
+
+  // Directories to ignore
+  ignoreDirs: [
+    '/old-version'
+  ],
+
+  // Files to ignore
   ignoreFiles: [
     'old-staff.css',
   ]
